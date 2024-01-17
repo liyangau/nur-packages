8eb50d652b70fe72ab8dc768f6b928adb0fb037e
liyangau <li.yang@konghq.com>
2024-01-18 00:01:05 +1100

    Revert "add yaml2nix"
    This reverts commit af5fe7b67a3d8a864a921101f38c7c31c5564bb5.

--- default.nix
+++ default.nix
@@ -5,20 +5,23 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}}: {
+
+{ pkgs ? import <nixpkgs> { } }:
+
+{
   # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib {inherit pkgs;}; # functions
+  lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  deck = pkgs.callPackage ./pkgs/deck {};
-  case-cli = pkgs.callPackage ./pkgs/case-cli {};
-  hexo-cli = pkgs.callPackage ./pkgs/hexo-cli {};
-  kong-portal-cli = pkgs.callPackage ./pkgs/kong-portal-cli {};
-  squoosh-cli = pkgs.callPackage ./pkgs/squoosh-cli {};
-  ingress2gateway = pkgs.callPackage ./pkgs/ingress2gateway {};
-  cloudflare-cli = pkgs.callPackage ./pkgs/cloudflare-cli {};
-  yaml2nix = pkgs.callPackage ./pkgs/yaml2nix {};
+  deck = pkgs.callPackage ./pkgs/deck { };
+  case-cli = pkgs.callPackage ./pkgs/case-cli { };
+  hexo-cli = pkgs.callPackage ./pkgs/hexo-cli { };
+  kong-portal-cli = pkgs.callPackage ./pkgs/kong-portal-cli { };
+  squoosh-cli = pkgs.callPackage ./pkgs/squoosh-cli { };
+  ingress2gateway = pkgs.callPackage ./pkgs/ingress2gateway { };
+  cloudflare-cli = pkgs.callPackage ./pkgs/cloudflare-cli { };
+  # yaml2nix = pkgs.callPackage ./pkgs/yaml2nix { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
